3fff515442a4b3152a3473298fad1133b21685ba
spacerovka <spacerovka@gmail.com>
2017-08-19 17:02:27 +0300

    service tests

---
+++ src/test/resources/menu.sql
@@ -0,0 +1,6 @@
+DELETE FROM menu;
+INSERT INTO menu (id,text, URL, status, menu_type) VALUES (1,"Home", "", true, "LEFT");
+INSERT INTO menu (id,text, URL, status, menu_type) VALUES (2,"About", "about", true, "LEFT");
+INSERT INTO menu (id,text, URL, status, menu_type) VALUES (3,"Contacts", "contacts", true, "LEFT");
+INSERT INTO menu (id,text, URL, status, menu_type) VALUES (4,"Delivery", "delivery", true, "RIGHT");
+INSERT INTO menu (id,text, URL, status, menu_type) VALUES (5,"Contact Us", "contact", true, "RIGHT");
